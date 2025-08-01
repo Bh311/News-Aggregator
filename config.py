68da068edf0747f4ad874e4189fc74a186f68b25
Aviralbh <116446271+Bh311@users.noreply.github.com>
2025-08-01 22:48:28 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,35 @@
+# RSS Feeds
+RSS_FEEDS = {
+    "india": [
+        "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+        "https://www.hindustantimes.com/feeds/rss/india-news/rssfeed.xml",
+        "https://indianexpress.com/section/india/feed/",
+        "https://www.thehindu.com/news/national/feeder/default.rss"
+    ],
+    "world": [
+        "http://feeds.bbci.co.uk/news/world/rss.xml",
+        "https://rss.cnn.com/rss/edition_world.rss",
+        "https://www.aljazeera.com/xml/rss/all.xml",
+        "https://www.reutersagency.com/feed/?best-topics=world&post_type=best",
+        "https://www.npr.org/rss/rss.php?id=1004"
+    ],
+    "ai": [
+        "https://ai.googleblog.com/feeds/posts/default",
+        "https://openai.com/blog/rss.xml",
+        "https://blogs.microsoft.com/ai/rss",
+        "https://www.technologyreview.com/topic/artificial-intelligence/feed",
+        "https://venturebeat.com/category/ai/feed/"
+    ]
+}
+
+
+# Twitter Configur
+TWITTER_API_KEY = ""
+TWITTER_API_SECRET = ""
+TWITTER_ACCESS_TOKEN = ""
+TWITTER_ACCESS_TOKEN_SECRET = ""
+TWITTER_LIST_ID = ""
+
+# Notion Configuration
+NOTION_API_KEY = ""
+NOTION_DATABASE_ID = ""
